6a7e1d985f10aac1560000e1a019774eb8e7277c
Eunhui <beensy74@gmail.com>
2024-06-03 19:16:18 -0400

    initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const TikTokApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color(0xFFE7626C),
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
+        primaryColor: const Color(0xFFE9435A),
       ),
-      home: const HomeScreen(),
+      home: Container(),
     );
   }
 }
